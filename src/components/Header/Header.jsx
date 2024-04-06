562cfab35e550779bc1f32807e68b0f2ddd18ed4
Marcos Morais <mmstec@gmail.com>
2024-04-06 11:44:47 -0300

    Finaizado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,27 @@
-import {useState, useEffect} from 'react'
-import { Col, Container,Row } from 'react-bootstrap'
-import headerImg from "../../assets/header-eva-e-wall.png"
-import "./Header.css"
+import React, { useState, useEffect } from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import headerImg from "../../assets/header-eva-e-wall.png";
+import "./Header.css";
 
 const Header = () => {
+  // Estados para controlar a animação do texto
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index,setIndex] = useState(1);
-  const toRotate = [ "Marcos Morais", "Full-Stack Developer" ];
+  const [index, setIndex] = useState(1);
+  // Array de textos a serem rotacionados
+  const toRotate = ["Marcos Morais...", "Full-Stack Developer!"];
   const period = 2000;
 
   useEffect(() => {
+    // Função para atualizar o texto a cada intervalo de tempo
     let ticker = setInterval(() => {
       tick();
     }, delta);
 
     return () => { clearInterval(ticker) };
-  }, [text])
+  }, [text, delta]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
@@ -43,33 +46,28 @@ const Header = () => {
     } else {
       setIndex(prevIndex => prevIndex + 1);
     }
-  }
-
-
-
+  };
 
   return (
     <section className='banner' id='home'>
       <Container>
-        <Row className="aligh-items-center">
+        <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>            
-              <div>
-                <span className="tagline">Bem Vindo ao meu Portfolio</span>
-                <h1>{`Saudações, eu sou `} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Marcos Morais", "Full-Stack Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>👋🏻 Estou em constante aprendizado, fascinado em contribuir para o desenvolvimento de pessoas, proporcionar melhorias para o mundo tecnológico e adquirir e compartilhar conhecimentos.</p>
-              </div>
-           
+            <div>
+              <span className="tagline">Bem Vindo ao meu Portfolio</span>
+              <h1>{`Saudações, eu sou `} <span className="txt-rotate" data-period="1000" data-rotate='["Marcos Morais", "Full-Stack Developer"]'><span className="wrap">{text}</span></span></h1>
+              <p>👋🏻 Estou em constante aprendizado, fascinado em contribuir para o desenvolvimento de pessoas, proporcionar melhorias para o mundo tecnológico e adquirir e compartilhar conhecimentos.</p>
+            </div>
           </Col>
           <Col xs={12} md={6} xl={5}>             
-           <div>
-            <img src={headerImg} alt="Header Img"/>
-           </div>           
+            <div>
+              <img src={headerImg} alt="Header Img"/>
+            </div>           
           </Col>
         </Row>
       </Container>
-
     </section>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/components/Header/HeaderSocial.jsx
+++ src/components/Header/HeaderSocial.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import {BsLinkedin} from "react-icons/bs"
-import {FaGithub} from "react-icons/fa"
-import {BsWhatsapp} from "react-icons/bs"
+import React from 'react';
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { BsWhatsapp } from "react-icons/bs";
 
 const HeaderSocial = () => {
   return (
     <div className='header_socials'>
-      <a href="https://www.linkedin.com/in/marcosmoraisjr/" target="_blank" rel="noreferrer"><BsLinkedin/></a>
-      <a href="https://github.com/marcosmoraisjr" target="_blank" rel="noreferrer"><FaGithub/></a>
-      <a href="https://api.whatsapp.com/send/?phone=5573981023484&text&app_absent=0" target="_blank" rel="noreferrer"><BsWhatsapp/></a>
-      
+      {/* Link para o perfil do LinkedIn */}
+      <a href="https://www.linkedin.com/in/marcosmoraisjr/" target="_blank" rel="noreferrer">
+        <BsLinkedin/>
+      </a>
+      {/* Link para o perfil do GitHub */}
+      <a href="https://github.com/marcosmoraisjr" target="_blank" rel="noreferrer">
+        <FaGithub/>
+      </a>
+      {/* Link para o WhatsApp com uma mensagem pré-definida */}
+      <a href="https://api.whatsapp.com/send/?phone=5573981023484&text&app_absent=0" target="_blank" rel="noreferrer">
+        <BsWhatsapp/>
+      </a>
     </div>
   )
 }
 
-export default HeaderSocial
+export default HeaderSocial;
